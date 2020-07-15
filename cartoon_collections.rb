dc28b7d3516cc425f9ad6403a68e9ce5ccbe430e
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-15 01:49:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,10 @@ def long_planeteer_calls(words)
   words.any? {|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese_type|
+  if snacks.include? cheese_type
+    return cheese_type
+    break
 end
